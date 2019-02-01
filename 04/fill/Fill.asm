a35da57683772fef84f001d673749a7cafec9801
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-01-31 17:44:40 -0800

    Made fill code more readable.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,52 +11,52 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 (WHILE_LOOP)
-@16384
-D=A
-@currentPosition
-M=D // currentPos is a pointer to the upper left corner of the screen
-@i
-M=0
-@KBD
-D=M
-@IF_TRUE
-D;JEQ // if no Keyboard Input, Goto IF_TRUE
+    @16384
+    D=A
+    @currentPosition
+    M=D // currentPos is a pointer to the upper left corner of the screen
+    @i
+    M=0 // Reset incrementer variable
+    @KBD
+    D=M
+    @IF_TRUE
+    D;JEQ // if no Keyboard Input, Goto IF_TRUE
 (LOOP) // for (int i = 0; i <= 8192; ++i)
-@i
-D=M
-@8192
-D=D-A
-@END
-D;JEQ
-@currentPosition
-A=M
-M=-1
-@currentPosition
-D = M
-D = D + 1
-M=D
-@i
-M = M + 1
-@LOOP
-0;JMP
+    @i
+    D=M
+    @8192
+    D=D-A
+    @END
+    D;JEQ
+    @currentPosition
+    A=M
+    M=-1
+    @currentPosition
+    D = M
+    D = D + 1
+    M=D
+    @i
+    M = M + 1
+    @LOOP
+    0;JMP
 (IF_TRUE) // for (int i = 0; i <= 8192; ++i)
-@i
-D=M
-@8192
-D=D-A
-@END
-D;JEQ
-@currentPosition
-A=M
-M=0
-@currentPosition
-D = M
-D = D + 1
-M=D
-@i
-M = M + 1
-@IF_TRUE
-0;JMP
+    @i
+    D=M
+    @8192
+    D=D-A
+    @END
+    D;JEQ
+    @currentPosition
+    A=M
+    M=0
+    @currentPosition
+    D = M
+    D = D + 1
+    M=D
+    @i
+    M = M + 1
+    @IF_TRUE
+    0;JMP
 (END)
-@WHILE_LOOP
-0;JMP
+    @WHILE_LOOP
+    0;JMP
