14ad1c48579b6ebcebfd233bb5fbe498b8289655
Garrett Gunnell <garrett99@bendbroadband.com>
2019-02-06 01:35:46 -0800

    Implemented parser with completed dest function.

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,3 +1,47 @@
+class Parser
+  @@file = nil
+
+  def initialize(file)
+    @@file = File.open(file)
+  end
+
+  def file()
+    return @@file
+  end
+
+  def command_type(line)
+    if line[0] != '@'
+      return 'C'
+    else
+      return 'A'
+    end
+  end
+
+  def symbol(line)
+  end
+
+  def dest(line)
+    dest = line.split('=')[0]
+    if dest == 'M'
+      return '001'
+    elsif dest == 'D'
+      return '010'
+    elsif dest == 'MD'
+      return '011'
+    elsif dest == 'A'
+      return '100'
+    elsif dest == 'AM'
+      return '101'
+    elsif dest == 'AD'
+      return '110'
+    elsif dest == 'AMD'
+      return '111'
+    else
+      return '000'
+    end
+  end
+end
+
 print "What .asm file would you like to convert? (Please provide the path) "
 input_file = gets.chomp
 
@@ -11,9 +55,12 @@ else
   exit
 end
 
-File.open(input_file).each do |line|
+parser = Parser.new(input_file)
+
+parser.file.each do |line|
   skip_line = false
   new_line = ''
+  dest = '('
   line.split(//).each do |x|
     if x == '('
       skip_line = true
@@ -24,5 +71,6 @@ File.open(input_file).each do |line|
     new_line += x
   end
   next if skip_line || new_line.strip.empty?
-  puts new_line
+  puts parser.dest(line)
+  # puts new_line
 end
