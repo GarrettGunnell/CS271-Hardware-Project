b61013bbe7e422d8a8e1e9e4bdf70945d5b130de
Garrett Gunnell <garrett99@bendbroadband.com>
2019-02-06 00:19:23 -0800

    Assembler now detects and removes comments and line breaks.

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -2,10 +2,9 @@ print "What .asm file would you like to convert? (Please provide the path) "
 input_file = gets.chomp
 
 if File.exists?(input_file)
-  if File.extname(input_file) == ".asm"
-    print "Your file is an .asm file\n"
-  else
+  unless File.extname(input_file) == ".asm"
     print "Your file is not an asm file\n"
+    exit
   end
 else
   print "I did not find the file.\n"
@@ -13,5 +12,17 @@ else
 end
 
 File.open(input_file).each do |line|
-  puts line
+  skip_line = false
+  new_line = ''
+  line.split(//).each do |x|
+    if x == '('
+      skip_line = true
+      break
+    elsif x == "/"
+      break
+    end
+    new_line += x
+  end
+  next if skip_line || new_line.strip.empty?
+  puts new_line
 end
