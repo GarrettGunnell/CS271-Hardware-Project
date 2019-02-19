f3bd8618511932603262b10da791adfa6dd62c98
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-02-18 17:31:16 -0800

    Actually passes StackTest now.

--- 07/VirtualMachine/code_writer.rb
+++ 07/VirtualMachine/code_writer.rb
@@ -33,6 +33,7 @@ class CodeWriter
       equality = @@equalities[command]
       label1 = "If_True" + @current_labels.to_s
       label2 = "Else" + @current_labels.to_s
+      label3 = "End" + @current_labels.to_s
       @output_file.puts "@SP\n"\
       "AM=M-1\n"\
       "D=M\n"\
@@ -46,10 +47,13 @@ class CodeWriter
       "@SP\n"\
       "A=M-1\n"\
       "M=-1\n"\
+      "@#{label3}\n"\
+      "0;JMP\n"\
       "(#{label2})\n"\
       "@SP\n"\
       "A=M-1\n"\
-      "M=0"
+      "M=0\n"\
+      "(#{label3})"
       @current_labels += 1
     elsif command == 'or' || command == 'and' # Handles X|Y and X&Y
       if command == 'or'
@@ -64,11 +68,11 @@ class CodeWriter
       "M=#{operator}"
     elsif command == 'not' #!Y
       @output_file.puts "@SP\n"\
-      "AM=M-1\n"\
+      "A=M-1\n"\
       "M=!M"\
     elsif command == 'neg' #-Y
       @output_file.puts "@SP\n"\
-      "AM=M-1\n"\
+      "A=M-1\n"\
       "M=-M"\
     end
     @output_file.puts "//#{current_line}"
@@ -151,7 +155,7 @@ class CodeWriter
     @output_file.puts "//#{current_line}"
   end
 
-  def close() # Puts infinite loop at end of asm file 
+  def close() # Puts infinite loop at end of asm file
     @output_file.puts "(END)\n"\
     "@END\n"\
     "0;JMP"
